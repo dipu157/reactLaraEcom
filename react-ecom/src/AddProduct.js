92d67d9259842f0729a60305ca10cfc974e4f127
dipu157 <dipu157@gmail.com>
2022-08-23 22:07:16 +0600

    add product api post

--- react-ecom/src/AddProduct.js
+++ react-ecom/src/AddProduct.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
+import {useNavigate} from 'react-router-dom'
 import Header from './Header'
 
 export default function AddProduct() {
+
+  const [name,setName] = useState("");
+  const [description,setDescription] = useState("");
+  const [price,setPrice] = useState("");
+  const [file,setFile] = useState("");
+  const history = useNavigate();
+
+  async function addProduct()
+  {
+    
+    console.warn(name,description,price,file);
+    const formData = new FormData();
+    formData.append('file',file);
+    formData.append('price',price);
+    formData.append('description',description);
+    formData.append('name',name);
+
+    let result = await fetch("http://localhost:8000/api/addproduct",{
+      method: 'POST',
+      body: formData
+    });
+    alert("Data Save Successfully");
+  }
+
   return (
-    <div>
+    <div className='col-sm-6 offset-sm-3'>
       <Header />
-      <h1>AddProduct</h1> 
+      <h1>Add Product</h1> 
+          <input type="text" onChange={(e) => setName(e.target.value)} className="form-control" placeholder="Name" /> <br />
+          <input type="text" onChange={(e) => setDescription(e.target.value)}  className="form-control" placeholder="Description" /> <br />
+          <input type="text" onChange={(e) => setPrice(e.target.value)} className="form-control" placeholder="Price" /> <br />
+          <input type="file" onChange={(e) => setFile(e.target.files[0])} className="form-control" placeholder="photo" /> <br />
+          <button onClick={addProduct} className='btn btn-primary'>ADD Product</button>
     </div>
   )
 }
--- reactEcom_backend/app/Http/Controllers/ProductController.php
+++ reactEcom_backend/app/Http/Controllers/ProductController.php
@@ -9,18 +9,17 @@ class ProductController extends Controller
 {
     public function addProduct(Request $request)
     {
+        $product = new Product;
+        $product->name = $request->input('name');
+        $product->price = $request->input('price');
+        $product->description = $request->input('description');
+        $product->file_path = $request->file('file')->store('products');
+        $product->save();
+        return $product;
+    }
 
-        $file = $request->file('file');
-
-        $data = [
-            'name' => $request->name,
-            'description' => $request->description,
-            'price' => $request->price,
-            'file_path' => $file,
-        ];
-
-        Product::create($data);
-
-        return $data;
+    public function list()
+    {
+        return Product::all();
     }
 }
